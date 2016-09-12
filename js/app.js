4636506e43893f9db7505b61967d3127a6bdb7ce
Carlos Matheu Armengol <info@plastikaweb.com>
2016-09-12 21:10:08 +0200

    Cat constructor

--- js/app.js
+++ js/app.js
@@ -1,16 +1,33 @@
 var ViewModel = function () {
+    var self = this;
+    this.currentCat = ko.observable(new Cat());
+    this.incrementCounter = function () {
+        self.currentCat().clickCount(self.currentCat().clickCount() + 1);
+    }
+};
+
+var Cat = function () {
     this.clickCount = ko.observable(0);
     this.name = ko.observable('Tabby');
-    this.level = ko.observable('Born');
+    this.nickNames = ko.observableArray([
+        'cute',
+        'ball',
+        'pitty',
+        'white'
+    ]);
     this.imgSrc = ko.observable('http://cosmouk.cdnds.net/15/42/1445004076-white-cat-bag.jpg');
 
-    this.incrementCounter = function() {
-        this.clickCount(this.clickCount() + 1);
-        if (this.clickCount() > 20) {
-            this.level('adult');
-        } else if(this.clickCount() > 10) {
-            this.level('infant');
+    this.title = ko.computed(function () {
+        var title;
+        var clicks = this.clickCount();
+        if (clicks > 20) {
+            title = 'adult';
+        } else if (clicks > 10) {
+            title = 'infant';
+        } else {
+            title = 'born';
         }
-    }
+        return title;
+    }, this);
 };
 ko.applyBindings(new ViewModel());
