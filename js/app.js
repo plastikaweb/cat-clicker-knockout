2275dae3c17697e556debb2b5c6dda6adfbaebb6
Carlos Matheu Armengol <info@plastikaweb.com>
2016-09-13 11:00:11 +0200

    list of cats array observable

--- js/app.js
+++ js/app.js
@@ -1,21 +1,52 @@
+var initialCats = [
+    {
+        clickCount: 0,
+        name: 'missy',
+        imgSrc: 'http://cosmouk.cdnds.net/15/42/1445004076-white-cat-bag.jpg',
+        nickNames: ['pitty', 'chucky', 'missy', 'cuqui']
+    },
+    {
+        clickCount: 0,
+        name: 'isir',
+        imgSrc: 'http://www.catster.com/wp-content/uploads/2015/06/can-cats-eat-peanut-butter-02.jpg',
+        nickNames: ['osris']
+    },
+    {
+        clickCount: 0,
+        name: 'garfield',
+        imgSrc: 'http://s5.favim.com/orig/51/animals-cats-cute-feline-photography-Favim.com-551634.jpg',
+        nickNames: ['orange', 'orange ball']
+    },
+    {
+        clickCount: 0,
+        name: 'chuck',
+        imgSrc: 'http://cosmouk.cdnds.net/15/42/1445004076-white-cat-bag.jpg',
+        nickNames: ['leprosy']
+    },
+    {
+        clickCount: 0,
+        name: 'rya',
+        imgSrc: 'http://stuffpoint.com/cats/image/87009-cats-cute-cat.jpg',
+        nickNames: ['sun god', 'pitty', 'sweet']
+    }
+];
 var ViewModel = function () {
     var self = this;
-    this.currentCat = ko.observable(new Cat());
+    this.catsList = ko.observableArray([]);
+    initialCats.forEach(function(cat) {
+       self.catsList.push(new Cat(cat));
+    });
+    this.currentCat = ko.observable(this.catsList()[0]);
     this.incrementCounter = function () {
         self.currentCat().clickCount(self.currentCat().clickCount() + 1);
     }
 };
 
-var Cat = function () {
-    this.clickCount = ko.observable(0);
-    this.name = ko.observable('Tabby');
-    this.nickNames = ko.observableArray([
-        'cute',
-        'ball',
-        'pitty',
-        'white'
-    ]);
-    this.imgSrc = ko.observable('http://cosmouk.cdnds.net/15/42/1445004076-white-cat-bag.jpg');
+var Cat = function (data) {
+    this.clickCount = ko.observable(data.clickCount);
+    this.name = ko.observable(data.name);
+    this.nickNames = ko.observableArray(data.nickNames);
+    this.imgSrc = ko.observable(data.imgSrc);
 
     this.title = ko.computed(function () {
         var title;
